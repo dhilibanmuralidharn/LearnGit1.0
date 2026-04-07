7ad9842c67c09780f2f42cb54093ee4bac2204ac
dhilip <dhilip.m@payhuddle.com>
2026-04-07 19:35:34 +0530

    stash changes

--- index.js
+++ index.js
@@ -4,9 +4,3 @@ for (let index = 0; index < array.length; index++) {
     const element = array[index];
     console.log('element')
 }
-
-// stash changes 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    console.log('element')
-}
