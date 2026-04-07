7363a80a2d9e63b79785fa87f39da72cf5bb4bc0
dhilip <dhilip.m@payhuddle.com>
2026-04-07 17:16:01 +0530

    commit after reset

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("git learnig")
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log('element')
+}
+
+console.log('commit as stage')
