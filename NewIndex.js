2f38ef091220dfc5a63b99dbeaeb96268002cb6a
dhilip <dhilip.m@payhuddle.com>
2026-04-08 15:57:35 +0530

    adderss conflict

--- NewIndex.js
+++ NewIndex.js
@@ -8,6 +8,11 @@ for (let index = 0; index < array.length; index++) {
     console.log("new Feature");
 }
 
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    clg("to adderss conflict");
+}
 for (let index = 0; index < array.length; index++) {
     const element = array[index];
     console.log("conflict check")
