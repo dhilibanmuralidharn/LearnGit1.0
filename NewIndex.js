1b6b38dab611ec7cb4e669dd68b715ce402b0057
dhilip <dhilip.m@payhuddle.com>
2026-04-08 15:56:04 +0530

    conflict

--- NewIndex.js
+++ NewIndex.js
@@ -7,3 +7,8 @@ for (let index = 0; index < array.length; index++) {
     const element = array[index];
     console.log("new Feature");
 }
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log("conflict check")
+}
