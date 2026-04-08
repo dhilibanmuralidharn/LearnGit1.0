5c6ea65b3758b3213d393e1b8d9bc1fe2a14f5c9
dhilip <dhilip.m@payhuddle.com>
2026-04-08 10:44:25 +0530

    produciton-bug-fix

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ for (let index = 0; index < array.length; index++) {
     const element = array[index];
 
 }
+
+
+// production issuse sloved
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+
+}
