01388064b6c2d030a7306f771943b373deed4512
dhilip <dhilip.m@payhuddle.com>
2026-04-08 15:42:56 +0530

    initial-newFeature

---
+++ NewIndex.js
@@ -0,0 +1,9 @@
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log("new Feature");
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log("new Feature");
+}
