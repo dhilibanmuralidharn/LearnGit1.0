467e8663aa1ab4ae1d690037ee38147536eb433e
dhilip <dhilip.m@payhuddle.com>
2026-04-08 11:36:11 +0530

    Revert "bad-coomit"
    This reverts commit 659abe5a85fc3e4eb7934912463264a09737ed31.

--- index.js
+++ index.js
@@ -30,9 +30,12 @@ for (let index = 0; index < array.length; index++) {
     const element = array[index];
 
 }
+<<<<<<< HEAD
 
 //Rebase example 
 for (let index = 0; index < 5; index++) {
     console.log(index);
 
 }
+=======
+>>>>>>> 38b34cc (Revert "bad-coomit")
