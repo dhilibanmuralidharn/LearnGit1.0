b91bd1ad3c47e198876206c30be506905b1b3efe
dhilip <dhilip.m@payhuddle.com>
2026-04-08 10:31:27 +0530

    Bug-fix

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 console.log("git learnig")
 
+// bug Fix
 for (let index = 0; index < array.length; index++) {
     const element = array[index];
-    console.log('element')
+    console.log('element', element);
+    console.log(i)
 }
