44321a8bc0dd5a71ab7d3311326ebe89e291f948
dhilip <dhilip.m@payhuddle.com>
2026-04-08 10:28:32 +0530

    today-1st-commit

--- index.js
+++ index.js
@@ -4,9 +4,3 @@ for (let index = 0; index < array.length; index++) {
     const element = array[index];
     console.log('element')
 }
-
-// stash 2
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    console.log('element')
-}
